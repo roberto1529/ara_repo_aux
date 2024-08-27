01490f6db89aeedce8b456f56b73c820d9b3f751
Roberto Molina Gonzalez <45692969+roberto1529@users.noreply.github.com>
2024-08-27 17:53:27 -0500

    update
    entrega final de comercializadora electrohuila

--- comercializadora_electrohuila.py
+++ comercializadora_electrohuila.py
@@ -1,194 +1,3 @@
-# import os
-# import time
-# import logging
-# import multiprocessing as mp
-# import fitz  # PyMuPDF
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from commons.commons import start_logging, read_excel_electrohuila, process_error
-# from funciones import generar_arbol_carpetas
-
-# # Configuración del logging
-# logger = start_logging("LOGS_EHUILA", mode="dev")
-# comercializadora = 'Electrohuila'
-# def configurar_navegador():
-#     """
-#     Configura el navegador Chrome para la descarga del PDF.
-#     """
-#     options = Options()
-#     options.add_argument("--no-sandbox")
-#     options.add_argument("--disable-dev-shm-usage")
-#     options.add_argument("--disable-gpu")
-#     options.add_argument("--disable-software-rasterizer")
-#     options.add_argument("--disable-web-security")
-#     options.add_argument("--allow-running-insecure-content")
-#     options.add_argument("--disable-blink-features=AutomationControlled")
-#     options.add_argument("--disable-extensions")
-#     options.add_argument("--disable-popup-blocking")
-#     options.add_argument("--disable-infobars")
-#     options.add_argument("--ignore-certificate-errors")
-#     options.add_argument("--allow-insecure-localhost")
-#     options.add_argument("--disable-features=InsecureDownloadWarnings")
-#     options.add_argument("--window-size=800x600")
-
-#     # Configuración de preferencias de descarga
-#     download_path = f'C:\\Users\\P108\\Documents\\PyDocto\\{comercializadora}'
-#     prefs = {
-#         "download.prompt_for_download": False,
-#         "download.directory_upgrade": True,
-#         "safebrowsing.enabled": True,
-#         "plugins.always_open_pdf_externally": True,
-#         "profile.managed_default_content_settings.images": 2,  # Bloquear imágenes
-#         "download.default_directory": download_path
-#     }
-#     options.add_experimental_option("prefs", prefs)
-    
-#     return options, download_path
-
-# def descargar_pdf(contrato):
-#     """
-#     Usa Selenium para descargar el PDF del contrato.
-#     """
-#     options, download_path = configurar_navegador()
-#     service = Service('chromedriver.exe')
-#     driver = webdriver.Chrome(service=service, options=options)
-
-#     logging.info("Navegador Chrome iniciado")
-
-#     try:
-#         # Carga la página local
-#         driver.get('https://enlinea.electrohuila.com.co/generate-invoice/')
-#         logging.info("Página cargada")
-
-#         # Espera a que el campo de matrícula esté presente y visible
-#         input_element = WebDriverWait(driver, 10).until(
-#             EC.presence_of_element_located((By.ID, 'txtAccount'))
-#         )
-#         conStr = str(contrato)
-#         input_element.send_keys(conStr)
-#         logging.info(f"Texto escrito en el input: {conStr}")
-
-#         # Espera a que el botón de consultar esté presente y visible
-#         consult_button = WebDriverWait(driver, 10).until(
-#             EC.presence_of_element_located((By.ID, 'btnSearch'))
-#         )
-#         consult_button.click()
-#         logging.info("Botón 'Consultar' clicado")
-
-#         # Espera a que el PDF se cargue completamente dentro del iframe
-#         time.sleep(15)  # Ajusta este tiempo según lo que tarde en cargar
-
-#         # Obtén el src del iframe y redirige a esa URL para descargar el PDF
-#         iframe = driver.find_element(By.ID, 'iframeInvoice')
-#         pdf_url = iframe.get_attribute('src')
-#         driver.get(pdf_url)
-#         logging.info(f"Redirigido al PDF en: {pdf_url}")
-
-#         # Espera un tiempo para asegurar que el PDF se descargue
-#         time.sleep(10)
-
-#     except Exception as e:
-#         logging.error(f"Ocurrió un error durante la descarga del PDF: {str(e)}")
-#     finally:
-#         # Cierra el navegador
-#         driver.quit()
-
-#     return os.path.join(download_path, f'{contrato}.pdf')
-
-# def leer_pdf(pdf_path):
-#     """
-#     Lee el contenido del PDF y busca el texto después de "Período".
-#     Detiene la búsqueda después de encontrar el primer resultado.
-#     """
-#     logging.info(f"Buscando el archivo PDF en: {pdf_path}")
-
-#     if not os.path.exists(pdf_path):
-#         logging.error(f"El archivo {pdf_path} no existe.")
-#         return None
-
-#     try:
-#         # Abre el archivo PDF
-#         pdf_document = fitz.open(pdf_path)
-#         logging.info("Archivo PDF abierto")
-
-#         # Itera sobre las páginas del PDF
-#         for page_num in range(len(pdf_document)):
-#             page = pdf_document.load_page(page_num)
-#             text = page.get_text()
-#             logging.info(f"Texto extraído de la página {page_num + 1}:\n{text}")
-
-#             # Busca el texto "Período" y extrae el texto que sigue
-#             periodo_index = text.find("Período")
-#             if periodo_index != -1:
-#                 start_index = periodo_index + len("Período")
-#                 # Extrae el texto que sigue a "Período" (ajusta el rango según el formato del PDF)
-#                 extracted_text = text[start_index:start_index + 100].strip()  # Ajusta el rango según sea necesario
-#                 logging.info(f"Texto encontrado después de 'Período': {extracted_text}")
-#                 return extracted_text  # Retorna el texto encontrado y sale del bucle
-                
-#             else:
-#                 logging.info("Texto 'Período' no encontrado en la página.")
-
-#         return None  # Si no se encuentra el texto "Período"
-
-#     except Exception as e:
-#         logging.error(f"Ocurrió un error al procesar el PDF: {str(e)}")
-#         return None
-#     finally:
-#         # Cierra el documento PDF
-#         pdf_document.close()
-
-# def mover_pdf(pdf_path, destino):
-#     """
-#     Mueve el archivo PDF a la nueva ubicación especificada por destino.
-#     """
-#     try:
-#         os.makedirs(os.path.dirname(destino), exist_ok=True)
-#         os.rename(pdf_path, destino)
-#         logging.info(f"PDF movido a: {destino}")
-#     except Exception as e:
-#         logging.error(f"No se pudo mover el archivo: {str(e)}")
-
-# def procesar_facturas(contrato):
-#     """
-#     Procesa un contrato descargando el PDF, extrayendo información relevante,
-#     generando una estructura de carpetas y moviendo el archivo PDF.
-#     """
-#     pdf_path = descargar_pdf(contrato)
-#     texto_encontrado = leer_pdf(pdf_path)
-
-#     if texto_encontrado:
-#         # Genera la estructura de carpetas basada en el texto encontrado
-#         nueva_ruta = generar_arbol_carpetas(texto_encontrado,comercializadora)
-#         destino = os.path.join(nueva_ruta, f'{contrato}.pdf')
-        
-#         # Mueve el PDF a la nueva ubicación
-#         mover_pdf(pdf_path, destino)
-#     else:
-#         logging.warning(f"No se encontró texto relevante en el PDF para el contrato {contrato}")
-
-# def download_contratos():
-#     """
-#     Recolecta todos los contratos y ejecuta la función 'procesar_facturas' usando multiprocessing.
-#     """
-#     df = read_excel_electrohuila()
-#     contratos = df["CONTRATO"].tolist()
-
-#     with mp.Pool(processes=1) as pool:
-#         pool.map(procesar_facturas, contratos)
-
-# if __name__ == "__main__":
-#     try:
-#         download_contratos()
-#     except Exception as e:
-#         process_error("warning")
-#         logger.error(f"Ocurrió un error al cargar la aplicación: {e}")
-
-
 import os
 import time
 import logging
@@ -200,28 +9,25 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from commons.commons import start_logging, read_excel_electrohuila, process_error
-from funciones import generar_arbol_carpetas
+from commons.commons import start_logging, read_excel_electrohuila, read_excel_homologacion
+from funciones import generar_arbol_carpetas, renombrar_pdf, conexion_correo, mover_pdf, eliminar_archivo_con_motivo, verificar_descarga, registrar_descarga
 
 # Configuración del logging
 logger = start_logging("LOGS_EHUILA", mode="dev")
 comercializadora = 'Electrohuila'
 
 def configurar_navegador():
+    """
+    Configura el navegador Chrome para la descarga del PDF.
+    """
     options = Options()
-    options.add_argument("--headless")  # Ejecuta en modo headless para ahorrar recursos
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=800x600")
-
     download_path = f'C:\\Users\\P108\\Documents\\PyDocto\\{comercializadora}'
     prefs = {
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True,
         "plugins.always_open_pdf_externally": True,
-        "profile.managed_default_content_settings.images": 2,
+        "profile.managed_default_content_settings.images": 2,  # Bloquear imágenes
         "download.default_directory": download_path
     }
     options.add_experimental_option("prefs", prefs)
@@ -229,32 +35,48 @@ def configurar_navegador():
     return options, download_path
 
 def descargar_pdf(contrato):
+    """
+    Usa Selenium para descargar el PDF del contrato si aún no ha sido descargado.
+    """
     options, download_path = configurar_navegador()
     service = Service('chromedriver.exe')
     driver = webdriver.Chrome(service=service, options=options)
+    log_file = os.path.join(download_path, 'descargas.log')
+    pdf_path = os.path.join(download_path, f'{contrato["CONTRATO"]}.pdf')
+
+    if verificar_descarga(contrato['CONTRATO'], download_path):
+        logging.info(f"El archivo para el contrato {contrato['CONTRATO']} ya ha sido descargado anteriormente.")
+        driver.quit()  # Asegúrate de cerrar el navegador si no se descarga el PDF
+        return pdf_path
+
     logging.info("Navegador Chrome iniciado")
 
     try:
+        # Carga la página local
         driver.get('https://enlinea.electrohuila.com.co/generate-invoice/')
+        logging.info("Página cargada")
+
+        # Interacción con la página...
         input_element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, 'txtAccount'))
         )
-        input_element.send_keys(str(contrato))
-        logging.info(f"Texto escrito en el input: {contrato}")
+        conStr = str(contrato['CONTRATO'])
+        input_element.send_keys(conStr)
+        logging.info(f"Texto escrito en el input: {conStr}")
 
+        # Clic en el botón de consulta...
         consult_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, 'btnSearch'))
         )
         consult_button.click()
         logging.info("Botón 'Consultar' clicado")
 
+        # Espera la carga del PDF y descarga...
         time.sleep(15)
-
         iframe = driver.find_element(By.ID, 'iframeInvoice')
         pdf_url = iframe.get_attribute('src')
         driver.get(pdf_url)
         logging.info(f"Redirigido al PDF en: {pdf_url}")
-
         time.sleep(10)
 
     except Exception as e:
@@ -262,17 +84,24 @@ def descargar_pdf(contrato):
     finally:
         driver.quit()
 
-    return os.path.join(download_path, f'{contrato}.pdf')
+    return pdf_path
 
 def leer_pdf(pdf_path):
+    """
+    Lee el contenido del PDF y busca el texto después de "Período".
+    """
     logging.info(f"Buscando el archivo PDF en: {pdf_path}")
+
     if not os.path.exists(pdf_path):
-        logging.error(f"El archivo {pdf_path} no existe.")
+        eliminar_archivo_con_motivo(pdf_path, "Factura no encontrada")
         return None
 
     try:
         pdf_document = fitz.open(pdf_path)
         logging.info("Archivo PDF abierto")
+
+        if len(pdf_document) == 0:
+            raise Exception("El archivo PDF está vacío o dañado.")
 
         for page_num in range(len(pdf_document)):
             page = pdf_document.load_page(page_num)
@@ -282,50 +111,66 @@ def leer_pdf(pdf_path):
             periodo_index = text.find("Período")
             if periodo_index != -1:
                 start_index = periodo_index + len("Período")
-                extracted_text = text[start_index:start_index + 100].strip()
+                extracted_text = text[start_index:start_index + 100].strip() 
                 logging.info(f"Texto encontrado después de 'Período': {extracted_text}")
-                return extracted_text
-                
+                return extracted_text  
             else:
                 logging.info("Texto 'Período' no encontrado en la página.")
 
         return None
 
     except Exception as e:
-        logging.error(f"Ocurrió un error al procesar el PDF: {str(e)}")
+        eliminar_archivo_con_motivo(pdf_path, "Factura no encontrada")
+        err = "Factura no encontrada: " + str(e)
+        conexion_correo('Error de ejecución en el bot', err);
         return None
     finally:
         pdf_document.close()
 
-def mover_pdf(pdf_path, destino):
-    try:
-        os.makedirs(os.path.dirname(destino), exist_ok=True)
-        os.rename(pdf_path, destino)
-        logging.info(f"PDF movido a: {destino}")
-    except Exception as e:
-        logging.error(f"No se pudo mover el archivo: {str(e)}")
 
 def procesar_facturas(contrato):
+    """
+    Procesa un contrato descargando el PDF, extrayendo información relevante,
+    renombrando el archivo y moviéndolo a la carpeta correspondiente.
+    """
     pdf_path = descargar_pdf(contrato)
-    texto_encontrado = leer_pdf(pdf_path)
+    if os.path.exists(pdf_path):
+        texto_encontrado = leer_pdf(pdf_path)
 
-    if texto_encontrado:
-        nueva_ruta = generar_arbol_carpetas(texto_encontrado, comercializadora)
-        destino = os.path.join(nueva_ruta, f'{contrato}.pdf')
-        mover_pdf(pdf_path, destino)
+        if texto_encontrado:
+            nuevo_nombre = renombrar_pdf(contrato['SAP'], contrato['Comercializadora'], texto_encontrado)
+            if nuevo_nombre:
+                nueva_ruta = generar_arbol_carpetas(texto_encontrado, comercializadora)
+                destino = os.path.join(nueva_ruta, nuevo_nombre)
+                ruta_final = mover_pdf(pdf_path, destino)
+
+                if ruta_final:  # Verifica si el archivo fue movido exitosamente
+                    log_file = os.path.join(nueva_ruta, 'descargas.log')
+                    registrar_descarga(contrato['CONTRATO'], ruta_final, log_file)
+        else:
+            # No se encontró texto relevante, pero no se registra el error
+            print(f"No se encontró texto relevante en el PDF para el contrato {contrato}")
     else:
-        logging.warning(f"No se encontró texto relevante en el PDF para el contrato {contrato}")
+        # El archivo PDF no se descargó correctamente, pero no se registra el error
+        print(f"El archivo PDF {pdf_path} no se descargó correctamente.")
 
 def download_contratos():
+    """
+    Recolecta todos los contratos y ejecuta la función 'procesar_facturas' usando multiprocessing.
+    """
     df = read_excel_electrohuila()
-    contratos = df["CONTRATO"].tolist()
-    
-    with mp.Pool(processes=mp.cpu_count()) as pool:
-        pool.map(procesar_facturas, contratos)
+    hm = read_excel_homologacion()
+    dfm = df.merge(hm, on='Supplier', how='left')
+
+    contratos = dfm.to_dict('records')  # Convertir DataFrame a lista de diccionarios
+
+    with mp.Pool(processes=1) as pool:
+        pool.starmap(procesar_facturas, [(contrato,) for contrato in contratos])
 
 if __name__ == "__main__":
     try:
         download_contratos()
     except Exception as e:
-        process_error("warning")
         logger.error(f"Ocurrió un error al cargar la aplicación: {e}")
+        err = "error de funcionamiento: " + str(e)
+        conexion_correo('Error de ejecución en el bot', err);
--- funciones.py
+++ funciones.py
@@ -121,57 +121,58 @@ def insertar_datos(insert_query,datos,tabla):
 
 
 
-def conexion_correo():
-    
-    asunto_notificacion = "Variables sin valor"
-    formato_del_correo = f"""El presente correo es para informar que las siguientes variables no fueron encontradas 
-    en el pdf (coloca nombre del pdf) perteneciente a la comercializadora: (nombre coercializadora)"""
-    cuerpo_notificacion = 1
-    
+def conexion_correo(asunto_notificacion, formato_del_correo, cuerpo_notificacion):
     try:
-
-        cuenta_correo = 'calvarez@ises.com.co'
-        # Inicializar cliente correo #
-        outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-
+       
+        cuenta_correo = 'amaldonado@ises.com.co'
+       
+        # Inicializar cliente de correo #
+        outlookApp = win32com.client.Dispatch("Outlook.Application")
+        outlookNamespace = outlookApp.GetNamespace("MAPI")
+       
         # Configurar bandeja/carpeta a revisar #
-        inbox = outlook.Folders(cuenta_correo).Folders("Bandeja de entrada")
-
-        # Envío de correo de notificación
-        outlookApp = win32com.Dispatch("Outlook.Application").GetNameSpace("MAPI")
-
-        mailItem = outlookApp.CreateItem(0)
+        inbox = outlookNamespace.Folders(cuenta_correo).Folders("Inbox")
+       
+        # Envío de correo de notificación #
+        mailItem = outlookApp.CreateItem(0)  # Crear el ítem de correo
         mailItem.Subject = asunto_notificacion
-        mailItem.BodyFormat = formato_del_correo
+        #mailItem.BodyFormat = formato_del_correo  # Asegúrate de que esto esté configurado adecuadamente
         mailItem.Body = cuerpo_notificacion
-
-        for correo in config["correo_remitente"]:
+       
+        # Agregar destinatarios #
+        for correo in config['correo']['remitente']:
             mailItem.Recipients.Add(correo)
-
+       
         mailItem.Save()
         mailItem.Send()
-    
-    except:
-        logging.warning(
-            f"Ocurrió un error",
-            tr.format_exc(),
-        )
+        logging.info("Correo enviado con éxito")
+   
+    except Exception as e:
+        logging.error("Ocurrió un error al enviar el correo: %s", str(e))
+        logging.exception("Detalles del error:")
+ 
+ 
 
-# funcion para generar arbol de carpetas
 def generar_arbol_carpetas(texto_fecha, comercializadora):
+    """
+    Genera la ruta de carpetas basándose en el texto_fecha y la comercializadora.
+    """
     if '-' in texto_fecha:
-        mes_texto = texto_fecha.split('-')[0]
+        periodo = texto_fecha.split('-')[0].strip()
     else:
-        mes_texto = texto_fecha[:3]
-
+        periodo = texto_fecha[:3].strip()  # Asume formato 'MMM' para el mes
+    
+    mes_texto = periodo  # Directamente usar el texto del mes
+    
     mes_numero = obtener_mes_numero(mes_texto)
 
     if not mes_numero:
+        logging.error(f"No se pudo determinar el mes a partir de {texto_fecha}")
         return None
 
-    anio_actual = datetime.datetime.now().year
+    anio_actual = datetime.datetime.now().year  # Obtener el año actual
 
-    ruta = os.path.join('C:\\Users\\P108\\Documents\\PyDocto\\',  str(comercializadora),  str(anio_actual),   f'{mes_numero:02d}')
+    ruta = os.path.join('C:\\Users\\P108\\Documents\\PyDocto\\', str(comercializadora), str(anio_actual), f'{mes_numero:02d}')
 
     if not os.path.exists(ruta):
         os.makedirs(ruta)
@@ -180,13 +181,13 @@ def generar_arbol_carpetas(texto_fecha, comercializadora):
         print(f"Directorio {ruta} ya existe")
 
     return ruta
-
 # funcion para generar arbol de carpetas / complemento para generar numero de mes de carperta
 def obtener_mes_numero(mes_texto):
     meses = {
         "ene": 1, "feb": 2, "mar": 3, "abr": 4, "may": 5, "jun": 6,
         "jul": 7, "ago": 8, "sep": 9, "oct": 10, "nov": 11, "dic": 12
     }
+    
 
     try:
         mes_numero = int(mes_texto)
@@ -198,6 +199,76 @@ def obtener_mes_numero(mes_texto):
     except ValueError:
         mes_texto = mes_texto.lower()[:3]
         return meses.get(mes_texto, None)
+
+def renombrar_pdf(sap, supplier, texto_fecha):
+    """
+    Renombra el archivo PDF basado en el año y mes extraído de texto_fecha.
+    """
+   
+    
+    # Extraer el período y el año
+    if '-' in texto_fecha:
+        periodo = texto_fecha.split('-')[0].strip()
+        anio = texto_fecha.split('-')[1].strip()  # Año después del guion
+    else:
+        periodo = texto_fecha[:7].strip()  # Asume el formato 'MM-YYYY' o 'MMM-YYYY'
+        anio = texto_fecha[7:].strip()  # Año después del período
+    
+    # Extraer mes y año del período
+    mes_texto = periodo[:3]  # Extrae el texto del mes
+    anio = anio[:4]  # Asegura que el año tenga cuatro dígitos
+    
+    # Convertir el mes a número
+    mes_numero = obtener_mes_numero(mes_texto)
+    if mes_numero:
+        mes_formateado = f"{mes_numero:02d}"  # Asegura que el mes tenga dos dígitos
+        nuevo_nombre = f"{sap}_{supplier}_{anio}{mes_formateado}.pdf"
+        return nuevo_nombre
+    else:
+        logging.error(f"No se pudo determinar el mes a partir de {texto_fecha}")
+        return None
+    
+
+def mover_pdf(pdf_path, destino):
+    """
+    Mueve el archivo PDF a la nueva ubicación especificada por destino.
+    """
+    try:
+        os.makedirs(os.path.dirname(destino), exist_ok=True)
+        os.rename(pdf_path, destino)
+        logging.info(f"PDF movido a: {destino}")
+        return destino  # Devuelve la ruta final del archivo
+    except Exception as e:
+        eliminar_archivo_con_motivo(pdf_path, str(e))
+        err = "Error al organizar factura: " + str(e)
+        conexion_correo('Error de ejecución en el bot', err);
+        return None
+
+def eliminar_archivo_con_motivo(pdf_path, motivo):
+    """
+    Elimina el archivo PDF y muestra un mensaje indicando el motivo, solo con el nombre del archivo.
+    """
+    try:
+        # Extrae solo el nombre del archivo desde la ruta
+        nombre_archivo = os.path.basename(pdf_path)
+        os.remove(pdf_path)
+        print(f"Archivo eliminado: {nombre_archivo}. Motivo: {motivo}")
+    except Exception as e:
+        print(f"No se pudo eliminar el archivo {nombre_archivo}: {str(e)}")
+
+def verificar_descarga(contrato, download_path):
+    """
+    Verifica si el contrato ya ha sido descargado consultando las carpetas.
+    """
+    pdf_path = os.path.join(download_path, f'{contrato}.pdf')
+    return os.path.exists(pdf_path)
+
+def registrar_descarga(contrato, pdf_path, log_file):
+    """
+    Registra el contrato descargado y la ubicación en el archivo de log.
+    """
+    with open(log_file, 'a') as log:
+        log.write(f"{contrato}.pdf,{pdf_path}\n")
 
 # def remonbrar_arc(archivo):
       # Esperar a que el archivo se descargue completamente
